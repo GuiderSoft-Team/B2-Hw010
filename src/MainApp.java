a874f3b53dc2beaac35a0a340dad44da21575194
burcuturanugur <107644861+burcuturanugur@users.noreply.github.com>
2022-12-07 20:10:14 +0300

    Burcu Uğur - Homework10 is done.

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,6 +1,13 @@
+
 public class MainApp {
     public static void main(String[] args) {
         //Lütfen readme.md dosyasını okuyunuz.
+
+        Tools t=new Tools();
+
+        System.out.println(t.splitWord("Burcu"));
+        System.out.println(t.splitWord("    "));
+
 
     }
 }
