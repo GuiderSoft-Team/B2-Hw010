a497c276b6e908537d0f9f98c0b3f4e8aa420dc8
serhat-project <sucar661@gmail.com>
2022-12-07 11:16:59 +0300

    Serhat Homework is done

---
+++ src/Tools.java
@@ -0,0 +1,36 @@
+public final class Tools {
+    public static String splitWord(String word) {
+        if (word == null || word.trim().length() == 0) {
+            throw new NullPointerException();
+        }
+        int middle = word.length() / 2;
+        String part1 = "";
+        String part2 = "";
+        try {
+            if (word.length() % 2 == 0) {
+                part1 = word.substring(0, middle);
+                part2 = word.substring(middle);
+            } else {
+                part1 = word.substring(0, middle + 1);
+                part2 = word.substring(middle + 1);
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Hata: Bir hata ile karşılaşıldı!");
+
+        }
+        return part1 + "-" + part2;
+    }
+
+    public static void main(String[] args) {
+        String word1 = "Ahmet";
+        String word2 = "Dursun";
+        String word3 = null;
+        String word4 = "     ";
+        System.out.println("Split Word1: " + splitWord(word1));
+        System.out.println("Split Word2: " + splitWord(word2));
+        System.out.println("Split Word3: " + splitWord(word3));
+        System.out.println("Split Word4: " + splitWord(word4));
+
+    }
+}
+
