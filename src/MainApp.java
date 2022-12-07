ab4cf749a6f31da8f354e3cf9265edb22d26fde8
YusufAmat <hoccayusuf@gmail.com>
2022-12-07 11:20:34 +0100

    B2-Hw010 ist erledigt.

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,6 +1,13 @@
 public class MainApp {
     public static void main(String[] args) {
         //Lütfen readme.md dosyasını okuyunuz.
-
+        try {
+            System.out.println(Tools.splitWord("Ahmet"));
+            System.out.println(Tools.splitWord("Dursun"));
+            //System.out.println(Tools.splitWord(null));
+            System.out.println(Tools.splitWord("  "));
+        } catch (NullPointerException ex) {
+            System.err.println("Hata olustu. " + ex.getMessage());;
+        }
     }
 }
