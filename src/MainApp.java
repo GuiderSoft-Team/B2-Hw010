cd83828d650f7667666963805cfef550249b53b7
özge topal <the.plus.fineart@gmail.com>
2022-12-07 14:21:28 +0300

    özge homework10

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,6 +1,18 @@
+
+
 public class MainApp {
     public static void main(String[] args) {
         //Lütfen readme.md dosyasını okuyunuz.
+
+
+        try {
+            Tools tool=new Tools();
+            System.out.println(tool.splitWord("özge"));
+
+        }catch (NullPointerException ex){
+            System.out.println("hata : "+ex.getMessage());
+
+        }
 
     }
 }
