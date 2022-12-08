fddcee826745ade5a7b4dcd81247b16a8bac149a
yasinmurat <muratyeni671@gmail.com>
2022-12-08 17:39:57 +0300

    Yasin Muarat Homework 10

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,6 +1,18 @@
 public class MainApp {
     public static void main(String[] args) {
         //Lütfen readme.md dosyasını okuyunuz.
+        try {
+            Tools tool=new Tools();
+            System.out.println(tool.splitWord("Murat"));
+            System.out.println(tool.splitWord("Yasin Murat"));
+            System.out.println(tool.splitWord(" "));
+
+        }catch (NullPointerException ex){
+            System.out.println("hata : "+ex.getMessage());
+
+        }
 
     }
-}
+
+    }
+
