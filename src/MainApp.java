0453a81a8f6f1d839a8a8e2090bafdca00e8a267
Erkan Cihan YILMAZ <therkancihan@gmail.com>
2022-12-08 19:33:14 +0300

    Erkan Cihan has completed. Dec. 8,2022

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,5 +2,10 @@ public class MainApp {
     public static void main(String[] args) {
         //Lütfen readme.md dosyasını okuyunuz.
 
+        System.out.println(Tools.splitWord("ErkanCihan"));
+        System.out.println(Tools.splitWord(" - "));
+        System.out.println(Tools.splitWord(""));
+
+
     }
 }
