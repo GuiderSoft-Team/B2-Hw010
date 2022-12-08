678f2d3cdfc4fa4ad7533510af7a67a90b06036e
Baristopcu08 <112826011+Baristopcu08@users.noreply.github.com>
2022-12-08 18:36:05 +0300

    B2-/H/w010

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,5 +2,7 @@ public class MainApp {
     public static void main(String[] args) {
         //Lütfen readme.md dosyasını okuyunuz.
 
+        System.out.println(Tool.splitWord(null)+"\n"+Tool.splitWord("Dursun"));
+
     }
 }
