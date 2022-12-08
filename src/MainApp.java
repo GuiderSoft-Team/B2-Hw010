86e4ef0f61d83fcc41b786b5321617b6ff752697
sule <komurcusulee3@gmail.com>
2022-12-08 19:44:16 +0300

    ŞULE KÖMÜRCÜ H-10

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,5 +2,13 @@ public class MainApp {
     public static void main(String[] args) {
         //Lütfen readme.md dosyasını okuyunuz.
 
+        System.out.println(Tools.splitWord("Tarık"));
+        System.out.println(Tools.splitWord(" "));
+        System.out.println(Tools.splitWord(""));
+
+
+
+
+
     }
 }
