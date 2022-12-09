9ad76e1c898bf593218047916ee49c42b6682036
suleymandurmaz <sdurmaz77@hotmail.com>
2022-12-09 10:35:56 +0300

    SULEYMAN HW0010 YAPILDI

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,6 +1,15 @@
 public class MainApp {
     public static void main(String[] args) {
         //Lütfen readme.md dosyasını okuyunuz.
+        //Lütfen readme.md dosyasını okuyunuz.
+        String split = Tools.splitWord("Dursun");
+        System.out.println(split);
+        String split1=Tools.splitWord("selahatin");
+        System.out.println(split1);
+
 
     }
 }
+
+
+
