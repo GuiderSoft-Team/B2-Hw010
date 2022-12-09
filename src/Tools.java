6db16c3ab26cb271aa4714cd41b8165879d5949a
ondergulsen <ondergulsenn@gmail.com>
2022-12-09 18:56:00 +0300

    Hw010

---
+++ src/Tools.java
@@ -0,0 +1,23 @@
+public class Tools {
+    String splitWord (String word)throws NullPointerException{
+        String part1="";
+        String part2="";
+        if ((word==null) || (word.isBlank())){
+            throw new NullPointerException("Kelime bos birakilamaz veya Null olamaz");
+        }
+        try {
+
+            if (word.length() % 2 == 0) {
+                part1 = word.substring(0, word.length() / 2);
+                part2 = word.substring(word.length() / 2);
+            } else {
+                part1 = word.substring(0, word.length() / 2 + 1);
+                part2 = word.substring(word.length() / 2 + 1);
+            }
+
+        }catch (StringIndexOutOfBoundsException ex){
+            ex.printStackTrace();
+            System.out.println(ex.getMessage());
+        }return part1 + "-" + part2;
+    }
+}
